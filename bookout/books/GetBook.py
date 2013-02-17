998691deed77000ccddbcfa7421097cf8c462284
thixson <t.hixson08@gmail.com>
2013-02-16 19:20:59 -0700

    Removed google books api key. Now you have to have a config file with one in it

--- bookout/books/GetBook.py
+++ bookout/books/GetBook.py
@@ -1,6 +1,7 @@
 from google.appengine.api import urlfetch
 from flask import jsonify
 import json
+from bookout.config import google_api_key
 
 def external_book_search(ISBN):
 	book = search_google_books(ISBN)
@@ -8,11 +9,10 @@ def external_book_search(ISBN):
 
 #Searches the google books api and returns a json object of the book or False if the book is not found
 def search_google_books(ISBN):
-	apiKey = "AIzaSyCX-e_MsPRJzpAc7rhQuh0W7pg2OGVVJ8A"
 	
 	#Search to get volumes associated with this isbn.
 	# There should only ever really be one returned, but if there are more than one, this returns the first
-	url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ISBN + "&key=" + apiKey
+	url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ISBN + "&key=" + google_api_key
 	response = urlfetch.fetch(url)
 	try:
 		if response.status_code == 200:
@@ -22,7 +22,7 @@ def search_google_books(ISBN):
 				for volume in data["items"]:
 					volumeID = volume["id"]
 					#For each volume returned, get the data on that book
-					url = "https://www.googleapis.com/books/v1/volumes/" + volumeID + "?key=" + apiKey
+					url = "https://www.googleapis.com/books/v1/volumes/" + volumeID + "?key=" + google_api_key
 					response = urlfetch.fetch(url)
 					if response.status_code == 200:
 						book_json = json.loads(response.content)
